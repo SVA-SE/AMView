1e571c5a610709b8305428f9042024ea2be7bfc5
Wiktor Gustafsson <wiktor.gustafsson@sva.se>
2023-12-18 10:56:01 +0100

    Move shinyapps-io to root and put deploy script in there

--- inst/deploy.R
+++
@@ -1,7 +0,0 @@
-scripts <- list.files("R", pattern = "[^(data)].R$", full.names = TRUE)
-
-file.copy(scripts, "inst/shinyapps-io/www", overwrite = TRUE)
-
-setwd("inst/shinyapps-io")
-
-rsconnect::deployApp(appName = "AMView", forceUpdate = TRUE)
